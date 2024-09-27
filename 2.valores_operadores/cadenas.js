5b4ebe19709f5f547cb70ecb73d5bd4be9c613ff
cantoral18 <cantoralmaritza6@gmail.com>
2024-09-27 11:02:51 -0500

    feet:clase sobre cadenas
    plantillas de literales uso de comillas simples,dobles y el caracter de escape

---
+++ 2.valores_operadores/cadenas.js
@@ -0,0 +1,31 @@
+
+//si nuestro texto tiene que estar entre comillas simples 
+console.log("´este texto se muestra entre comillas simples'")
+//si nuestro texto tiene que estar entre comillas dobles
+console.log(`"este texto se muestra en comillas dobles"`)
+//si nuestro texto tiene que estar entre comillas dobles como simples
+console.log(`'esta es comilla simple' ,"esta es comilla doble"`)
+//usando caracter de escape
+console.log("de este texto solo esta \"palabra\" estara en comillas ")
+// salto de linea usando acentos
+console.log(`
+    aqui una linea
+    aqui otra linea`)
+//salto de linea antes es6
+console.log("esta es la primera linea \n y esta es la segunda linea")
+//tabulacion con acento grabe
+console.log(`
+este texto no esta tabulado
+        este texto esta tabulado
+`)
+//tabulando con el caracter dde escape
+console.log("si tabular \t y tabulado")
+console.log("esta es una barra invertida \\")
+//concatenando cadenas
+console.log("hola"+" "+"mundo")
+//usando plantillas literales
+console.log(`la suma de 1+1 es = ${1+1}`)
+//otro ejercio:
+numeroUno=8
+numeroDos=9
+console.log(`la suma de ${numeroUno}+${numeroDs]es = ${1+1}`)
