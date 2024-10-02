08155049ce50c3006cea5951458368c1c61393ad
cantoral18 <cantoralmaritza6@gmail.com>
2024-10-02 13:15:32 -0500

    feet :clases de opTenario

---
+++ 2.valores_operadores/opTernario.js
@@ -0,0 +1,12 @@
+respuesta=5>7?"no es mayor":"como va ser mayor"
+console.log(respuesta)
+//ejercicio
+//usando el operador terciario evaluar si un numero es par o inpar
+respuesta =5 ? "es par":"o impar"
+console.log(respuesta)
+
+//mostrar un mensaje si la edad de una persona si es mayor a 18
+respuesta=20?"eres mayor":"eres menor"
+console.log(respuesta)
+
+//
